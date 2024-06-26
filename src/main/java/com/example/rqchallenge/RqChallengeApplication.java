f75e89557a0b3dcb2aef5dee4ade05d3fc676cf3
Onkar Raut <rautonkar3@gmail.com>
2024-06-26 21:18:33 +0530

    Update logging format
    Updated logging format for local and prod envs

--- src/main/java/com/example/rqchallenge/RqChallengeApplication.java
+++ src/main/java/com/example/rqchallenge/RqChallengeApplication.java
@@ -3,11 +3,10 @@ package com.example.rqchallenge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.rqchallenge"})
 public class RqChallengeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RqChallengeApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(RqChallengeApplication.class, args);
+  }
 }
